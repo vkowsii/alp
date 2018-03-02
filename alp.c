2f1ba686bce5fa13477a303684ad5b1179dbae2b
vkowsii <30829409+vkowsii@users.noreply.github.com>
2018-03-02 15:35:55 +0530

    Create alp.c

---
+++ alp.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char s1;
+printf("enter the string");
+scanf("%c",&s1);
+if((s1>'a'&&s1<'z')||(s1>0&&s1<a))
+{
+printf("yes");
+}
+else
+{
+printf("no");
+}
+}
